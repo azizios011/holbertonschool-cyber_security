7616af896ce5636eb200d9930acb111e9eb8d6e5
azizios011 <hdriaziz699@gmail.com>
2024-06-22 18:02:37 +0100

    Who is it ?

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 whois $1 | awk -F: '/Registrant Name|Registrant Organization|Registrant Street|Registrant City|Registrant State\/Province|Registrant Postal Code|Registrant Country|Registrant Phone|Registrant Email|Admin Name|Admin Organization|Admin Street|Admin City|Admin State\/Province|Admin Postal Code|Admin Country|Admin Phone|Admin Email|Tech Name|Tech Organization|Tech Street|Tech City|Tech State\/Province|Tech Postal Code|Tech Country|Tech Phone|Tech Email/ {print $1 "," $2}' > $1.csv
-
